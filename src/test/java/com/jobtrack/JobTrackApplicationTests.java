e55bbba0003b9e0a4551feebb22ffa15f553b3b5
KPM <67306285+its-kpm@users.noreply.github.com>
2026-09-13 00:01:46 -0500

    Add basic application test

---
+++ src/test/java/com/jobtrack/JobTrackApplicationTests.java
@@ -0,0 +1,10 @@
+package com.jobtrack;
+
+import org.junit.jupiter.api.Test;
+
+class JobTrackApplicationTests {
+    @Test
+    void applicationClassExists() {
+        JobTrackApplication.class.getName();
+    }
+}
